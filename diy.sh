20069e5d952c5d5d997dd71a8cd9e1dbf553c126
vexheleleldzode <heiliaopa@gmail.com>
2026-03-09 16:24:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# 添加 Passwall 2 及其依赖的最新源码源
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+# 添加 Passwall 2 专属依赖包源码
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# 添加 Passwall 2 核心源码
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
